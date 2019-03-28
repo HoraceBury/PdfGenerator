f30714d1fb7623e0726e01df10701abae2c343c1
Matthew Webster <webstem1@airproducts.com>
2019-03-28 12:12:13 +0000

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.IO;
+
+using DinkToPdf;
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //var str = GenerateHtml();
+            GeneratePdf();
+        }
+
+        public static string GenerateHtml()
+        {
+            return Razor2Pdf.RazorRenderer.DoHtml();
+        }
+
+        public static void GeneratePdf()
+        {
+            var slnpath = $@"{Directory.GetCurrentDirectory()}\..\..\..\..";
+            var htmlpath = $@"{slnpath}\HtmlTemplates\HTMLPage1.html";
+            var pdfpath = $@"{slnpath}\PdfFiles\Azam.pdf";
+            var dllpath = $@"{slnpath}\DinkNative64bit\libwkhtmltox.dll";
+
+            var html = new StringBuilder(File.ReadAllText(htmlpath));
+
+            var _converter = new SynchronizedConverter(new PdfTools());
+
+            var context = new CustomAssemblyLoadContext().LoadUnmanagedLibrary(dllpath);
+
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report",
+                //Out = @"D:\PDFCreator\Employee_Report.pdf"  USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
+            };
+            
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = html.ToString(),
+                //Page = "https://code-maze.com/", USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
+                WebSettings = { DefaultEncoding = "utf-8" }, //, UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" },
+                
+            };
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+
+            //_converter.Convert(pdf); IF WE USE Out PROPERTY IN THE GlobalSettings CLASS, THIS IS ENOUGH FOR CONVERSION
+
+            var file = _converter.Convert(pdf);
+
+            File.WriteAllBytes(pdfpath, file);
+
+            //return Ok("Successfully created PDF document.");
+            //return File(file, "application/pdf", "EmployeeReport.pdf"); //USE THIS RETURN STATEMENT TO DOWNLOAD GENERATED PDF DOCUMENT
+            //return File(file, "application/pdf");
+        }
+    }
+}
---
+++ ConsoleApp1/Razor2Pdf/RazorRenderer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Razor2Pdf
+{
+    public class MyModel { }
+
+    public class RazorRenderer
+    {
+        public static string DoHtml()
+        {
+            var slnpath = $@"{Directory.GetCurrentDirectory()}\..\..\..\..";
+            var razorpath = $@"{slnpath}\RazorPages";
+            var viewname = "MyView";
+
+            var renderer = GetRenderer();
+            var html = renderer.RenderViewToString(viewname, new MyModel());
+            return html;
+        }
+
+        private static RazorViewToStringRenderer GetRenderer()
+        {
+            var services = new ServiceCollection();
+            var applicationEnvironment = PlatformServices.Default.Application;
+            services.AddSingleton(applicationEnvironment);
+
+            var appDirectory = Directory.GetCurrentDirectory();
+
+            var environment = new HostingEnvironment
+            {
+                ApplicationName = Assembly.GetEntryAssembly().GetName().Name
+            };
+            services.AddSingleton<IHostingEnvironment>(environment);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(new PhysicalFileProvider(appDirectory));
+                options.FileProviders.Add(new PhysicalFileProvider($@"{appDirectory}\..\..\..\"));
+                //options.FileProviders.Add(new PhysicalFileProvider($@"{appDirectory}\..\..\..\..\RazorPages"));
+            });
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+
+            services.AddLogging();
+            services.AddMvc();
+            services.AddSingleton<RazorViewToStringRenderer>();
+
+            var provider = services.BuildServiceProvider();
+            return provider.GetRequiredService<RazorViewToStringRenderer>();
+        }
+    }
+    public class RazorViewToStringRenderer
+    {
+        private IRazorViewEngine _viewEngine;
+        private ITempDataProvider _tempDataProvider;
+        private IServiceProvider _serviceProvider;
+
+        public RazorViewToStringRenderer(
+            IRazorViewEngine viewEngine,
+            ITempDataProvider tempDataProvider,
+            IServiceProvider serviceProvider)
+        {
+            _viewEngine = viewEngine;
+            _tempDataProvider = tempDataProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+        public string RenderViewToString<TModel>(string viewName, TModel model)
+        {
+            var actionContext = GetActionContext();
+            var view = FindView(actionContext, viewName);
+
+            using (var output = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    actionContext,
+                    view,
+                    new ViewDataDictionary<TModel>(
+                        metadataProvider: new EmptyModelMetadataProvider(),
+                        modelState: new ModelStateDictionary())
+                    {
+                        Model = model
+                    },
+                    new TempDataDictionary(
+                        actionContext.HttpContext,
+                        _tempDataProvider),
+                    output,
+                    new HtmlHelperOptions());
+
+                view.RenderAsync(viewContext);
+
+                return output.ToString();
+            }
+        }
+
+        private IView FindView(ActionContext actionContext, string viewName)
+        {
+            var getViewResult = _viewEngine.GetView(executingFilePath: null, viewPath: viewName, isMainPage: true);
+            if (getViewResult.Success)
+            {
+                return getViewResult.View;
+            }
+
+            var findViewResult = _viewEngine.FindView(actionContext, viewName, isMainPage: false);
+            if (findViewResult.Success)
+            {
+                return findViewResult.View;
+            }
+
+            var searchedLocations = getViewResult.SearchedLocations.Concat(findViewResult.SearchedLocations);
+            var errorMessage = string.Join(
+                Environment.NewLine,
+                new[] { $"Unable to find view '{viewName}'. The following locations were searched:" }.Concat(searchedLocations)); ;
+
+            throw new InvalidOperationException(errorMessage);
+        }
+
+        private ActionContext GetActionContext()
+        {
+            var dict = new RouteValueDictionary();
+            dict.Add("razor", @"C:\git\Dink2Pdf\ConsoleApp1\RazorPages");
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.RequestServices = _serviceProvider;
+            return new ActionContext(httpContext, new RouteData(dict), new ActionDescriptor());
+        }
+    }
+}
